bde21b28904ac7461abf4b0ee83e015ee15da76c
Xinia <neko_1996@mail.ru>
2024-03-02 10:34:44 -0600

    Added toast and started animation of downloading

--- apps/frontend/src/app/page.jsx
+++ apps/frontend/src/app/page.jsx
@@ -1,16 +1,42 @@
 'use client'
 
 import axios from 'axios';
-import React, { useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import { Button, Box } from '@mui/material';
 import styles from './index.module.scss';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const Index = () => {
+  const [percentage, setPercentage] = useState(0);
+  const [progress, setProgress] = useState(null);
+
   const fetchAds = useCallback(() => {
+    const documentStyles = document.documentElement.style;
+    let progress = 0;
+    setProgress('in-progress');
     const fetchData = async () => {
-      const { data } = await axios.get('/api/ads?minPrice=1000000');
-      console.log(data);
+      const data = await axios({
+        url: '/api/ads?minPrice=1000000',
+        onDownloadProgress(progressEvent) {
+          progress = Math.round((progressEvent.loaded / progressEvent.total) * 100);
+
+          setPercentage(progress);
+
+          documentStyles.setProperty('--progress', `${progress}%`);
+      }
+      })
+      .then(res => {
+        setProgress('finished');
+        if (res.status === 200)
+        toast("Success!");
+      console.log(res);
+        })
+     .catch(err => {
+      toast("Error!");
+      console.log(err);
+     });
     }
     fetchData();
   }, []);
@@ -22,7 +48,10 @@ const Index = () => {
         <Button onClick={fetchAds} variant='outlined'>
           Send an API request
         </Button>
+        <span className="button-text">{progress === 'finished' ? '🎉 Done ' : ' '}</span>
+        <span className="percentage">{percentage}%</span>
       </Box>
+      <ToastContainer />
     </div>
   );
 }
